ebaddeb0ddb665d19b74aa8909ff135cc28b8dae
Wilson Weber <sanjabian.ho@gmail.com>
2023-04-20 21:00:33 +0430

    added Texts collection for Better Readability

---
+++ tel/Texts.py
@@ -0,0 +1,21 @@
+
+
+START_MESSAGE = """
+Hey!
+Welcome to linkedin scrapper, are you annoyed by linkedin terrible filters? don't worry, I've got your back!
+I have a very cool feature in this bot, I have called it Nested Logical Expression Filter Query.
+In a very simple language, you can write your filters and query in pure PYTHON expression!
+\n\nFor example, I want to get hird in netherlands where to work as backend engineer with django or fastapi, if there's a vuejs i can work as frontend too, or even full stack, also I want to only work in germany, so how do I write such hard query?
+Too easy! ```(django or fastapi) and (backend or (fullstack and vuejs) or (frontend and vuejs)) and germany```
+You only need 1 day of python knowledge to write your own logical expression :)
+Please use /show command to see all the possible namespaces and keywords you can filter.
+You may use /info to check your current query
+
+If you want to setup your filter, simply write it to me here.
+
+Remember, You must be a member of main channel to be able to use this bot:)
+@linkedin_python
+
+Also, don't forget to star my project in github or contribute if you found it cool/useful :)
+\n https://github.com/ManiMozaffar/linkedIn-scraper
+"""
