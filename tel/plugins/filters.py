6cd246ea9430cb67a3e39b4f9d21547cf0f88acb
Mani <mani.mozaffar@gmail.com>
2023-04-28 18:00:51 +0300

    Fixed telegram issue with chat member lookup

--- tel/plugins/filters.py
+++ tel/plugins/filters.py
@@ -1,6 +1,4 @@
-from functools import wraps
-
-
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram import enums
 from pyrogram.types import Message
 import loguru
@@ -11,14 +9,17 @@ loguru.logger.info(env.channel_id)
 
 
 async def is_user_a_member_of_channel(bot: app, message: Message) -> bool:
-    user = await bot.get_chat_member(
-        int(env.channel_id), message.from_user.id
-    )
-    return user.status in [
-        enums.ChatMemberStatus.ADMINISTRATOR,
-        enums.ChatMemberStatus.MEMBER,
-        enums.ChatMemberStatus.OWNER,
-    ]
+    try:
+        user = await bot.get_chat_member(
+            int(env.channel_id), message.from_user.id
+        )
+        return user.status in [
+            enums.ChatMemberStatus.ADMINISTRATOR,
+            enums.ChatMemberStatus.MEMBER,
+            enums.ChatMemberStatus.OWNER,
+        ]
+    except UserNotParticipant:
+        return False
 
 
 async def is_not_user_a_member_of_channel(bot: app, message: Message) -> bool:
