bf318eceafb186b230c773597f74f909f38842da
Mani <mani.mozaffar@gmail.com>
2023-05-13 19:00:29 +0300

    fixed: fastapi-integration's adoption's bug

--- src/core/config.py
+++ src/core/config.py
@@ -1,10 +1,14 @@
 from functools import lru_cache
 from dotenv import load_dotenv
-
 from fastapi_integration import FastApiConfig
+
+
+class MyConfig(FastApiConfig):
+    telegram_chat_id: str
+    telegram_token: str
 
 
 @lru_cache
 def get_app_settings() -> FastApiConfig:
     load_dotenv()
-    return FastApiConfig()
+    return MyConfig()
